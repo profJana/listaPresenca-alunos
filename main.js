8abfe5a81e1930edf09aafd08c9d06ab44b4e5e6
Janaina Tavares <112178884+profJana@users.noreply.github.com>
2023-09-04 17:11:17 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+
+//criar o array
+
+function submit() {
+
+    var name1 = document.getElementById("nome1").value;
+    var name2 = document.getElementById("nome2").value;
+    //nome3
+    //nome 4
+
+    //dar push em cada nome
+
+
+    document.getElementById("displayName").innerHTML = ;
+
+
+}
+
+function sorting() {
+    //ordenar
+    
+    document.getElementById("displayName").innerHTML = ;
+}
